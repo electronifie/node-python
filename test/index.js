09e34d5a27c021215e80fcdb23bde84e9619fbc0
Matt Walters <matt.walters@electronifie.com>
2014-11-24 14:46:13 -0500

    fixing incorrect test

--- test/index.js
+++ test/index.js
@@ -104,10 +104,10 @@ describe('node-python', function () {
   });
   it('should convert python lists to javascript arrays', function () {
     test = python.import('test2');
-    var value = test.getPythonValue([ '1', '2', '3']);
-    value.should.containEql(1);
+    var value = test.getPythonValue([ '1', 2, '3']);
+    value.should.containEql('1');
     value.should.containEql(2);
-    value.should.containEql(3);
+    value.should.containEql('3');
   });
   it('should convert python strings to javascript strings', function () {
     test = python.import('test2');
